9a852613855a6f6a7d05cdcf401c609a4b0bf4cd
Sarah <sara616b@stud.kea.dk>
2022-03-26 11:09:28 +0100

    correct db path

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from email import message
 from bottle import default_app, get, static_file, run, view
 import sqlite3
 
@@ -15,29 +14,38 @@ def script():
 @get("/")
 @view("index.html")
 def _():
-      
+
     message = "Database is not connected"
     # connect to database
-    db = sqlite3.connect("./database/database.db") or None
+    try:
+      import production
+      db = sqlite3.connect("/home/buzzer/site/database/database.db")
+    except:
+      print("not production")
+      db = sqlite3.connect("./database/database.db")
 
-    if db:
-          
-      message = "Database is connected"
-      # get tweet info from database
-      user = db.execute("""
-          SELECT *
-          FROM users
-          WHERE user_id = :user_id
-      """,(1,)).fetchone()
-    
-    if user:
-      message += f" and SELECTED this user: {user[1]}"
-      user = {
-        "user_id": user[0],
-        "user_display_name": user[1],
-        "user_username": user[2],
-        "user_email": user[3],
-      }
+    try:
+      if db:
+
+        message = "Database is connected"
+        # get tweet info from database
+        user = db.execute("""
+            SELECT *
+            FROM users
+            WHERE user_id = :user_id
+        """,(1,)).fetchone()
+
+      if user:
+        message += f" and SELECTED this user: {user[1]}"
+        user = {
+          "user_id": user[0],
+          "user_display_name": user[1],
+          "user_username": user[2],
+          "user_email": user[3],
+        }
+
+    except Exception as ex:
+      print(ex)
     
 
     return dict(message=message)
